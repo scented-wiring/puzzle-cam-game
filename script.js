7eb16fc1391a6fc7b61db8e8ba0a6444414ed837
Tom Hammersley <thomas.j.hammersley@gmail.com>
2021-11-02 21:01:22 +0000

    video fits to device size and scales automaticallyg

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 let VIDEO = null;
 let CANVAS = null;
 let CONTEXT = null;
+let SCALER = 0.8;
+let SIZE = { x: 0, y: 0, width: 0, height: 0 };
 
 const main = () => {
   CANVAS = document.getElementById("myCanvas");
   CONTEXT = CANVAS.getContext("2d");
-  CANVAS.width = window.innerWidth;
-  CANVAS.height = window.innerHeight;
 
   let promise = navigator.mediaDevices.getUserMedia({ video: true });
   promise
@@ -16,6 +16,8 @@ const main = () => {
       VIDEO.play();
 
       VIDEO.onloadeddata = function () {
+        handleResize();
+        window.addEventListener("resize", handleResize);
         updateCanvas();
       };
     })
@@ -24,7 +26,24 @@ const main = () => {
     });
 };
 
+const handleResize = () => {
+  CANVAS.width = window.innerWidth;
+  CANVAS.height = window.innerHeight;
+
+  let resizer =
+    SCALER *
+    Math.min(
+      window.innerWidth / VIDEO.videoWidth,
+      window.innerHeight / VIDEO.videoHeight
+    );
+  SIZE.width = resizer * VIDEO.videoWidth;
+  SIZE.height = resizer * VIDEO.videoHeight;
+  SIZE.x = window.innerWidth / 2 - SIZE.width / 2;
+  SIZE.y = window.innerHeight / 2 - SIZE.height / 2;
+};
+
 const updateCanvas = () => {
-  CONTEXT.drawImage(VIDEO, 0, 0);
+  CONTEXT.drawImage(VIDEO, SIZE.x, SIZE.y, SIZE.width, SIZE.height);
+
   window.requestAnimationFrame(updateCanvas);
 };
